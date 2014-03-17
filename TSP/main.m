3697d753c7333b23f05bb00255217ff76ac20fe0
Javier-varez <javier@tecnigraf.com>
2014-03-17 13:44:57 -0400

    Initial Commit

---
+++ TSP/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TSP
+//
+//  Created by Francisco Javier Álvarez García on 17/03/14.
+//  Copyright (c) 2014 Francisco Javier Álvarez García. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
